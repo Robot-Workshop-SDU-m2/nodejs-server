ed0d790fddae18b53bec58b0e57ccc70a04320c1
lyderik <benjamin.lyderik@gmail.com>
2018-04-16 12:02:59 +0200

    More 3d

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ var SerialPort = require('serialport');
 
 var counter = 0;
 var port;
+
+var choords = [[2000,0,0],[1950,0,0],[1900,0,0],[1850,0,0],[1800,0,0],[1750,0,0],[1700,0,0],[1750,0,0],[1800,0,0],[1850,0,0],[1900,0,0],[1950,0,0]];
+var pointer = 0;
+var clength = choords.length;
+
 SerialPort.list().then(
     data => {
         for(var i = 0; i < data.length; i++){
             if(data[i].manufacturer == 'FTDI'){
-                 port = new SerialPort(data[i].comName,{baudRate: 115200}, function (err) {
+                 port = new SerialPort(data[i].comName,{baudRate: 500000}, function (err) {
                     if (err) {
                         return console.log('Error: ', err.message);
                     }
@@ -17,9 +22,22 @@ SerialPort.list().then(
                 port.on('data', buf => {
                    strings = buf.toString().split('\n');
                    strings.forEach(str =>{
-                    console.log(counter++ + " : " + str);
+                    if(str.length != 0){
+                        console.log(counter++ + " : " + str.length + " : " + str );
+                        if(str == 'OK'){
+                            pointer = (pointer+1)%clength;
+                            console.log("G0;" + choords[pointer][0] + ";" + choords[pointer][1] + ";" + choords[pointer][2] + ";\n");
+                            port.write("G0;" + choords[pointer][0] + ";" + choords[pointer][1] + ";" + choords[pointer][2] + ";\n");
+                        }
+                    }
                    });
                 });
+                /*port.write("G0;" + choords[pointer][0] + ";" + choords[pointer][1] + ";" + choords[pointer][2] + ";\n");
+                pointer = (pointer+1)%clength;
+                port.write("G0;" + choords[pointer][0] + ";" + choords[pointer][1] + ";" + choords[pointer][2] + ";\n");
+                pointer = (pointer+1)%clength;
+                port.write("G0;" + choords[pointer][0] + ";" + choords[pointer][1] + ";" + choords[pointer][2] + ";\n");*/
+                
             }
         }
         if(data.length = 0){
